39f732114c6d314df7b5d740dd16090dd7696e75
Francesco Cosentino <francesco.cosentino@ing.com>
2023-12-15 13:56:14 +0100

    added version handling

--- bump-version.bash
+++ bump-version.bash
@@ -10,3 +10,10 @@ bump_version() {
 }
 
 bump_version && git add bridge/release.py
+
+
+readme() {
+    git describe --tags --abbrev=0 | awk -F. -v OFS=. '{$NF = $NF + 1;} 1' | sed 's/ /./g' | xargs -I {} sed -i '' 's/ghcr.io/hyp3rd/bridge:.*/ghcr.io/hyp3rd/bridge:"'{}'"/' README.md
+}
+
+readme && git add README.md
